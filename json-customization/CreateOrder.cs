53d7cfc4c43fa8c08a716bd2b43d69bf1713667d
mauroservienti <mauro.servienti@gmail.com>
2019-05-03 11:09:51 +0200

    Sample using Json.NET customization

---
+++ json-customization/CreateOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NServiceBus;
+
+public class CreateOrder :
+    IMessage
+{
+    public CreateOrder(int orderId, DateTime date, int customerId)
+    {
+        OrderId = orderId;
+        Date = date;
+        CustomerId = customerId;
+    }
+
+    public int OrderId { get; private set; }
+    public DateTime Date { get; private set; }
+    public int CustomerId { get; private set; }
+}
---
+++ json-customization/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NServiceBus;
+
+static class Program
+{
+    static async Task Main()
+    {
+        Console.Title = "Samples.Serialization";
+
+        var endpointConfiguration = new EndpointConfiguration("Samples.Serialization");
+        var settings = new JsonSerializerSettings
+        {
+            ContractResolver = new NonPublicPropertiesResolver(),
+            ConstructorHandling = ConstructorHandling.AllowNonPublicDefaultConstructor
+        };
+
+        var serialization = endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+        serialization.Settings(settings);
+
+        endpointConfiguration.UsePersistence<LearningPersistence>();
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        var endpointInstance = await Endpoint.Start(endpointConfiguration)
+            .ConfigureAwait(false);
+
+        var message = new CreateOrder(orderId: 9, date: DateTime.Now, customerId: 12);
+        await endpointInstance.SendLocal(message)
+            .ConfigureAwait(false);
+
+
+        Console.WriteLine("Order Sent");
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+        await endpointInstance.Stop()
+            .ConfigureAwait(false);
+    }
+}
